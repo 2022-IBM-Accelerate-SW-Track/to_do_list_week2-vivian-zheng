b47e3e329ea3e21e50ebf6b9d05b776ece6073b0
vivian-zheng <vivian.x.zheng@gmail.com>
2022-06-19 12:14:31 -0600

    changes to all files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,13 +20,32 @@ class Home extends Component {
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
+
+    // Go through each of the todos in the todos list
+    // Compare the content of each todo currently in the list (t) with the content of the todo wanting to be added (passed in as a parameter)
+    if(this.state.todos.find(t => t.content === todo.content)) {
+      return;
+    } else {
+      // Create a array that contains the current array and the new todo item
+      let new_list = [...this.state.todos, todo];
+
+      // Update the local state with the new array.
+      this.setState({
+        todos: new_list,
+      });
+    }
+
+  };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
     this.setState({
-      todos: new_list,
+      todos: todos,
     });
   };
+
   render() {
     return (
       <div className="Home">
@@ -36,7 +55,9 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+
+        
       </div>
     );
   }
